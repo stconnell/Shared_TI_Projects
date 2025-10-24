72492090ca76eda1716bc20b41959ee45f8ae7f4
stconnell <steve.james.connell@gmail.com>
2025-10-24 14:28:29 -0700

    Add files via upload

---
+++ sntp/docs/doxygen/html/search/all_3.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['sntp_2eh',['sntp.h',['../sntp_8h.html',1,'']]],
+  ['sntp_5fforcetimesync',['SNTP_forceTimeSync',['../sntp_8h.html#a8e06abb0bea13ea1365276ddf943a2ac',1,'sntp.h']]],
+  ['sntp_5fsetservers',['SNTP_setServers',['../sntp_8h.html#a32359003b7407532a6f75314004092cc',1,'sntp.h']]],
+  ['sntp_5fstart',['SNTP_start',['../sntp_8h.html#ac4bb57fdc40fcacd1fab2761978bea86',1,'sntp.h']]],
+  ['sntp_5fstop',['SNTP_stop',['../sntp_8h.html#aaab818e780a8ec9e87aa39a4d9a31f89',1,'sntp.h']]]
+];
